001743470b4596f659c22b3ec6d3739fa98964c1
iwhitest <ianjameswhitestone@gmail.com>
2017-03-18 16:14:02 -0400

    load toronto FSAs. default colouring

--- app/PostalLoader.py
+++ app/PostalLoader.py
@@ -8,16 +8,28 @@ shapefile = './app/data/pshape/CanadaPostalCodePolygons.shp'
 shapefile = './app/data/arcgis/gfsa000b11a_e.shp'
 
 
-def get_fsa():
+def getCanadaFSA():
     print ('Parsing shapefiles for %s' % shapefile)
     layer = fiona.open(shapefile)
-    # print (layer['geometries'])
 
+    fsa_list = []
     for f in layer:
         d = dict(f['properties'])
-        if d['PRNAME'] == 'Ontario' and d['CFSAUID'] == 'M4Y':
-            return f['geometry']
 
+        if d['PRNAME'] == 'Ontario' and d['CFSAUID'][0] == 'M':
+            geom = f['geometry']
+
+            ## convert tuple coordinates to list coordinates
+            if geom['type'] == 'Polygon':
+                # print (geom)
+                pass
+            elif geom['type'] == 'MultiPolygon':
+                coordinates = [[[list(tuple_coord) for tuple_coord in polygon[0]]] for polygon in geom['coordinates']]
+                geom['coordinates'] = coordinates
+            fsa_list.append(geom)
+    return fsa_list
+
+# get_fsa()
 
 # with fiona.drivers():
 #     for layername in fiona.listlayers('./pshape'):
