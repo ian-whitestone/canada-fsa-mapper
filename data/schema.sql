fbd5d1d2121127779b40b12de69ba188e56abe01
Ian Whitestone <ianjameswhitestone@gmail.com>
2017-06-24 22:17:17 -0400

    Update schema.sql

---
+++ data/schema.sql
@@ -0,0 +1,55 @@
+CREATE TABLE fsa_polys (
+  fsa varchar(3) PRIMARY KEY,
+  province varchar(100),
+  poly_str text,
+  poly geography(POLYGON,4326),
+  center_text text,
+  center geography(POINT,4326)
+)
+;
+
+
+CREATE TABLE fsa_multi_polys (
+  fsa varchar(3) PRIMARY KEY,
+  province varchar(100),
+  multi_poly_str text,
+  multi_poly geography(MULTIPOLYGON,4326),
+  center_text text,
+  center geography(POINT,4326)
+)
+;
+
+
+-- run after inserting poly_strs
+UPDATE fsa_polys
+SET poly = ST_GeomFromText(poly_str)
+;
+
+UPDATE fsa_polys
+SET center_text = ST_AsText(ST_Centroid(poly_str))
+;
+
+UPDATE fsa_polys
+SET center = ST_GeomFromText(center_text)
+;
+
+UPDATE fsa_multi_polys
+SET multi_poly = ST_GeomFromText(multi_poly_str)
+;
+
+UPDATE fsa_multi_polys
+SET center_text = ST_AsText(ST_Centroid(multi_poly_str))
+;
+
+UPDATE fsa_multi_polys
+SET center = ST_GeomFromText(center_text)
+;
+
+CREATE VIEW all_fsa AS (
+  SELECT fsa, province, ST_AsGeoJSON(ST_ForceRHR(poly::geometry)) AS geom, ST_AsGeoJSON(center) AS center
+  FROM fsa_polys
+  UNION ALL
+  SELECT fsa, province, ST_AsGeoJSON(ST_ForceRHR(multi_poly::geometry)) AS geom, ST_AsGeoJSON(center) AS center
+  FROM fsa_multi_polys
+)
+;
